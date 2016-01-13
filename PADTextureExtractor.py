e1e1cb502848052b66e6206282a4bc21eb3851e0
codywatts <codywatts.github@gmail.com>
2016-01-13 02:11:45 -0700

    Simplified texture-writing w/ new "Encoding" class.

--- PADTextureExtractor.py
+++ PADTextureExtractor.py
@@ -16,83 +16,55 @@ import sys
 import zipfile
 import zlib
 
+class Encoding:
+	def __init__(self, channels = None, packedPixelFormat = None, hasAlpha = None, isGreyscale = None):
+		self.channels = channels
+		if self.channels:
+			self.stride = (sum(self.channels) / 8)
+			self.hasAlpha = (len(self.channels) == 4)
+			self.isGreyscale = (len(self.channels) == 1)
+			self.packedPixelFormat = dict({4: ">{}L", 2: "<{}H", 1: "<{}B"})[self.stride]
+		else:
+			self.stride = None
+			self.hasAlpha = None
+			self.isGreyscale = None
+			self.packedPixelFormat = None
+
+R8G8B8A8 = Encoding([8, 8, 8, 8])
+R5G6B5 = Encoding([5, 6, 5])
+R4G4B4A4 = Encoding([4, 4, 4, 4])
+R5G5B5A1 = Encoding([5, 5, 5, 1])
+L8 = Encoding([8])
+JPEG = Encoding()
+
 class Texture:
-	def __init__(self, width, height, name, byteCount, encoding):
+	def __init__(self, width, height, name, buffer, encoding):
 		self.width = width
 		self.height = height
 		self.name = name
-		self.byteCount = byteCount
-		self.encoding = encoding
-
-	def setBuffer(self, buffer):
 		self.buffer = buffer
-
-	def getSizeInBytes(self):
-		if self.encoding == 0x0: return self.width * self.height * 4 # Four bytes per pixel
-		elif self.encoding == 0x2: return self.width * self.height * 2 # Two bytes per pixel
-		elif self.encoding == 0x3: return self.width * self.height * 2 # Two bytes per pixel
-		elif self.encoding == 0x4: return self.width * self.height * 2 # Two bytes per pixel
-		elif self.encoding == 0x8 or self.encoding == 0x9: return self.width * self.height # One byte per pixel
-		elif self.encoding == 0xD: return self.byteCount # Variable number of bytes per pixel
-		return 0
+		self.encoding = encoding
 
 	def exportToImageFile(self, fileName, outputDirectory):
 		if not os.path.isdir(outputDirectory):
 			os.makedirs(outputDirectory)
 		
 		with open(os.path.join(outputDirectory, fileName), 'wb') as outputFile:
-			# Encoding 0x0 is four bytes per pixel; one byte per red, green, blue and alpha channel.
-			if self.encoding == 0x0:
-				unpackedImageData = struct.unpack("<" + str(self.width * self.height * 4) + "B", self.buffer)
-				w = png.Writer(self.width, self.height, alpha=True)
-				w.write_array(outputFile, unpackedImageData)
-			
-			# Encoding 0x2 is two bytes per pixel; five bits for the red channel, six bits for the green channel, and five bits for the blue channel.
-			elif self.encoding == 0x2:
-				unpackedImageData = struct.unpack("<" + str(self.width * self.height) + "H", self.buffer)
-				imageByteArray = []
-				for pixel in unpackedImageData:
-					redByte = int(round(((pixel & 0xF800) >> 11) * (255 / 31)))
-					greenByte = int(round(((pixel & 0x07E0) >> 5) * (255 / 63)))
-					blueByte = int(round(((pixel & 0x001F)) * (255 / 31)))
-					imageByteArray.extend([redByte, greenByte, blueByte])
-				w = png.Writer(self.width, self.height)
+			if self.encoding != JPEG:
+				packedPixelFormat = self.encoding.packedPixelFormat.format(self.width * self.height)
+				unpackedImageData = struct.unpack(packedPixelFormat, self.buffer)
+				imageByteArray = [0] * self.width * self.height * len(self.encoding.channels)
+				for pixelIndex, pixelValue in enumerate(unpackedImageData):
+					for channelIndex, bitCount in reversed(list(enumerate(self.encoding.channels))):
+						bitMask = ((2 ** bitCount) - 1)
+						multiplier = float(255) / float(bitMask)
+						channelByte = int(round((pixelValue & bitMask) * multiplier))
+						pixelValue = pixelValue >> bitCount
+						imageByteArray[pixelIndex * len(self.encoding.channels) + channelIndex] = channelByte
+				w = png.Writer(self.width, self.height, alpha=self.encoding.hasAlpha, greyscale=self.encoding.isGreyscale)
 				w.write_array(outputFile, imageByteArray)
 			
-			# Encoding 0x3 is two bytes per pixel; four bits per red, green, blue and alpha channel.
-			elif self.encoding == 0x3:
-				unpackedImageData = struct.unpack("<" + str(self.width * self.height) + "H", self.buffer)
-				imageByteArray = []
-				for pixel in unpackedImageData:
-					redByte = ((pixel & 0xF000) >> 12) * 17
-					greenByte = ((pixel & 0x0F00) >> 8) * 17
-					blueByte = ((pixel & 0x00F0) >> 4) * 17
-					alphaByte = ((pixel & 0x000F) >> 0) * 17
-					imageByteArray.extend([redByte, greenByte, blueByte, alphaByte])
-				w = png.Writer(self.width, self.height, alpha=True)
-				w.write_array(outputFile, imageByteArray)
-			
-			# Encoding 0x4 is two bytes per pixel; five bits per red, green, blue channel, and then one bit for the alpha channel.
-			elif self.encoding == 0x4:
-				unpackedImageData = struct.unpack("<" + str(self.width * self.height) + "H", self.buffer)
-				imageByteArray = []
-				for pixel in unpackedImageData:
-					redByte = int(round(((pixel >> 11) & 0x001F) * (255 / 31)))
-					greenByte = int(round(((pixel >> 6) & 0x001F) * (255 / 31)))
-					blueByte = int(round(((pixel >> 1) & 0x001F) * (255 / 31)))
-					alphaByte = (pixel & 0x0001) * 255
-					imageByteArray.extend([redByte, greenByte, blueByte, alphaByte])
-				w = png.Writer(self.width, self.height, alpha=True)
-				w.write_array(outputFile, imageByteArray)
-			
-			# Encodings 0x8 and 0x9 are one byte per pixel; they are greyscale images.
-			elif self.encoding == 0x8 or self.encoding == 0x9:
-				unpackedImageData = struct.unpack("<" + str(self.width * self.height) + "B", self.buffer)
-				w = png.Writer(self.width, self.height, greyscale=True)
-				w.write_array(outputFile, unpackedImageData)
-			
-			# Encoding 0xD represents a raw JPEG image.
-			elif self.encoding == 0xD:
+			else:
 				outputFile.write(self.buffer)
 
 def decryptAndDecompressBinaryBlob(binaryBlob):
@@ -132,23 +104,39 @@ def extractTexturesFromBinaryBlob(binaryBlob, outputDirectory):
 			
 			startingOffset, width, height, name = struct.unpack(textureManifestFormat, binaryBlob[textureManifestStart:textureManifestEnd])
 			
-			encoding = (width >> 12)
+			encodingIdentifier = (width >> 12)
 			width = width & 0x0FFF
 			height = height & 0x0FFF
 			
+			encodings = dict()
+			# Encoding 0x0 is four bytes per pixel; one byte per red, green, blue and alpha channel.
+			encodings[0x0] = R8G8B8A8
+			# Encoding 0x2 is two bytes per pixel; five bits for the red channel, six bits for the green channel, and five bits for the blue channel.
+			encodings[0x2] = R5G6B5
+			# Encoding 0x3 is two bytes per pixel; four bits per red, green, blue and alpha channel.
+			encodings[0x3] = R4G4B4A4
+			# Encoding 0x4 is two bytes per pixel; five bits per red, green, blue channel, and then one bit for the alpha channel.
+			encodings[0x4] = R5G5B5A1
+			# Encodings 0x8 and 0x9 are one byte per pixel; they are greyscale images.
+			encodings[0x8] = L8
+			encodings[0x9] = L8
+			# Encoding 0xD represents a raw JPEG image.
+			encodings[0xD] = JPEG
+			
+			encoding = encodings[encodingIdentifier]
+			
 			byteCount = 0
-			if (encoding == 0xD):
-				byteCount = struct.unpack("I", name[-4:])[0]
-				name = name[:-4]
+			if (encoding != JPEG):
+				byteCount = width * height * encoding.stride
+			else:
+				name, byteCount = struct.unpack("20sI", name)
 			
 			name = name.rstrip('\0')
 			
-			image = Texture(width, height, name, byteCount, encoding)
-			
 			imageDataStart = offset + startingOffset
-			imageDataEnd = imageDataStart + image.getSizeInBytes()
+			imageDataEnd = imageDataStart + byteCount
 			
-			image.setBuffer(binaryBlob[imageDataStart:imageDataEnd])
+			image = Texture(width, height, name, binaryBlob[imageDataStart:imageDataEnd], encoding)
 			image.exportToImageFile(str(offset) + "_" + image.name, outputDirectory + " textures")
 		
 		offset = binaryBlob.find("TEX", offset + 1)
